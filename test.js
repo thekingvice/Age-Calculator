dc0b843c06be681abb803408a89cc1145f57d3b1
thekingvice <villarrealvicente92@gmail.com>
2023-04-16 00:03:56 -0400

    commit

---
+++ test.js
@@ -0,0 +1,16 @@
+const dd = 15;
+const mm = 4;
+const yy = 2001;
+const month_days = [];
+var today = new Date();
+
+const ageDays = 0;
+const ageMonths = 0;
+const ageYears = 0;
+
+function ageCalculator(day, month, year) {
+  const dob = new Date(year, month - 1, day);
+  const now = new Date();
+}
+
+console.log(ageCalculator(dd, mm, yy));
